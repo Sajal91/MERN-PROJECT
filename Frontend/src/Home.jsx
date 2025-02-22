23869a3dd47e72d01342863114c0db560b7509dc
rohannrai <rairohan2005@gmail.com>
2025-02-23 00:32:10 +0530

    Updates: 1. Test Commit

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,7 +1,7 @@
 const Home = function() {
     return (
         <>
-            <h1 className='text-center text-5xl font-bold uppercase'>Hey Welcome to my Ecom Project...</h1>
+            <h1 className='text-center text-5xl font-bold uppercase'>Hey i am rohan Welcome to my Ecom Project...</h1>
         </>
     )
 }
