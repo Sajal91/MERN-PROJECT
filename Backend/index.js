e1da05770cca893fe047ab30cbc859ed843cfd18
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-03-04 23:37:52 +0530

    Updated: 1. Backend Setup Initiated

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+main()
+.then(console.log("Database Connected Successfully"))
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGOURL);
+}
+
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (request, response) => {
+    console.log(response.send("Welcome to Home Route"));
+});
+
+app.listen(port, () => {
+    console.log("Server started on localhost :", port);
+})
