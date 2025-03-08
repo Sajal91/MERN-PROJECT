3203e056d72a1f672c836185b97c93d084350f53
rohannrai <rairohan2005@gmail.com>
2025-03-09 01:35:21 +0530

    Updates 1. Product page setup

--- Frontend/src/components/Product/Product.jsx
+++ Frontend/src/components/Product/Product.jsx
@@ -5,11 +5,15 @@ import { useParams } from "react-router"
 const Product = () => {
     const { productId } = useParams();
     const [isLoading, setIsLoading] = useState(true)
+    const [productdata , setProductdata] = useState({})
+    const [productimage , setProductimage] = useState("")
 
     useEffect(() => {
         fetchData.getProduct(productId)
             .then((data) => {
                 console.log(data)
+                setProductdata({...data})
+                
                 setIsLoading(false)
             }).catch((error) => {
                 console.log(error)
@@ -18,9 +22,23 @@ const Product = () => {
     }, [])
 
     return (
-        <>
-            <h1 className='text-center text-5xl font-bold uppercase'>Product Page...</h1>
-        </>
+             <>
+             <div className="absolute top-24 left w-full h-screen overflow-scroll scroll-smooth">
+             <div>
+              { productdata.Product_Image.map((image , index) => 
+              <div className="w-30 h-40"> 
+              <img src={image} key={index}  className="" /> 
+              </div>
+                  )}
+             </div>
+               <div>
+               <h3>  {productdata.Product_Name} </h3>
+               <h6> ${productdata.Product_Price} </h6>
+               <p>{ productdata.Product_Descroption }</p>
+               </div>
+               
+             </div>
+             </>
     )
 }
 
