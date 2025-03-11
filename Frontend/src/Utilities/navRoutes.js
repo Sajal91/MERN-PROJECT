3ea8b4a639238a55cb5dd712053e2880e8778b88
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-03-12 00:26:56 +0530

    Updates: 1. Typesript Branch Content removed

--- Frontend/src/Utilities/navRoutes.js
+++
@@ -1,8 +0,0 @@
-const navRoutes = [
-    { name: "Home", path: "/" },
-    { name: "About", path: "/about" },
-    { name: "Collection", path: "/collection" },
-    { name: "Login", path: "/login" }
-]
-
-export default navRoutes
