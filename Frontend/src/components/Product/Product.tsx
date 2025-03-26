eb47371618821ac84d22e9e360f299a1cd93dcdf
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-03-27 00:29:57 +0530

    Update: 1. Product Page Code Reoptimised

--- Frontend/src/components/Product/Product.tsx
+++ Frontend/src/components/Product/Product.tsx
@@ -2,11 +2,9 @@ import { FC, useEffect, useState } from "react"
 import fetchData from "../../utilities/fetchData"
 import { useParams } from "react-router"
 import ProductDataInterface from "../../interfaces/productDataInterface";
+import sizes from "../../constants/productSizes";
 
 
-
-
-const sizes: string[] = ["S", "M", "L", "XL"];
 
 const Product: FC = () => {
     const { productId } = useParams<string>()
@@ -69,63 +67,62 @@ const Product: FC = () => {
                             <p>{productdata?.Product_Description}</p>
                         </div>
                     </div>
-                <div className="flex w-[full] flex-wrap justify-center">
-                    <div className="flex w-1/2 flex-wrap">
-                    <div>
-                        {
-                            productdata?.Product_Image &&
-                            productdata.Product_Image.map((image, index) => {
-                                return <div key={index}>
-                                    <img src={image} alt={productdata.Product_Name} className="bg-gray-200 h-[100px] w-[150px] overflow-hidden" onClick={()=> onProductImage(image)} />
-                                </div>
-                            })
-                        }
+                    <div className="flex w-[full] flex-wrap justify-center">
+                        <div className="flex w-1/2 flex-wrap">
+                            <div>
+                                {
+                                    productdata?.Product_Image &&
+                                    productdata.Product_Image.map((image, index) => {
+                                        return <div key={index}>
+                                            <img src={image} alt={productdata.Product_Name} className="bg-gray-200 h-[100px] w-[150px] overflow-hidden" onClick={() => onProductImage(image)} />
+                                        </div>
+                                    })
+                                }
 
-                    </div>
-                    <div >
-                    <img src={productImage} alt="Selected Product" className="h-[60vh] w-[25vw]  bg-white p-5" />
+                            </div>
+                            <div >
+                                <img src={productImage} alt="Selected Product" className="h-[60vh] w-[25vw]  bg-white p-5" />
 
-                  </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 max-w-[600px]">
+                            <h1 className="text-2xl mt-10 ">{productdata?.Product_Name}</h1>
+                            <h6 className="mt-5"> Reviews</h6>
+                            <h6 className="mt-5 text-2xl font-bold ">${productdata?.Product_Price}</h6>
+                            <p className="mt-5 font-extralight ">{productdata?.Product_Description}</p>
+                            <h1 className="mt-5"> Select Size </h1>
+                            <div className="flex gap-3 mt-5">
+                                {sizes.map((size) => (
+                                    <label key={size} className="relative">
+                                        <input
+                                            type="radio"
+                                            name="size"
+                                            value={size}
+                                            checked={selectedSize === size}
+                                            onChange={(e) => setSelectedSize(e.target.value)}
+                                            className="hidden"
+                                        /><div
+                                            className={`w-12 h-12 flex items-center justify-center border-2 text-lg font-semibold cursor-pointer transition-all 
+                    ${selectedSize === size
+                                                    ? "border-orange-500"
+                                                    : "bg-white text-gray-800 border-gray-400 hover:bg-gray-100"
+                                                }`}
+                                        >
+                                            {size}
+                                        </div>
+                                    </label>
+
+                                ))}
+                            </div>
+                            <button className=" mt-8 bg-black text-amber-100 w-[150px] h-[40px] cursor-pointer"> Add To Cart </button>
+                            <hr className="mt-10 font-extralight" />
+                            <div className="mt-3 text-sm font-extralight">
+                                <p> 100% Original product.</p>
+                                <p>Cash on delivery is available on this product.</p>
+                                <p>Easy return and exchange policy within 7 days.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-1/2 max-w-[600px]">
-                        <h1 className="text-2xl mt-10 ">{productdata?.Product_Name}</h1>
-                        <h6 className="mt-5"> Reviews</h6>
-                        <h6 className="mt-5 text-2xl font-bold ">${productdata?.Product_Price}</h6>
-                        <p className="mt-5 font-extralight ">{productdata?.Product_Description}</p>
-                        <h1 className="mt-5"> Select Size </h1>
-                        <div className="flex gap-3 mt-5">
-                        {sizes.map((size) => (
-                        <label key={size} className="relative">
-                        <input
-                        type="radio"
-                        name="size"
-                        value={size}
-                        checked={selectedSize === size}
-                        onChange={(e) => setSelectedSize(e.target.value)}
-                        className="hidden"
-                    /><div
-                    className={`w-12 h-12 flex items-center justify-center border-2 text-lg font-semibold cursor-pointer transition-all 
-                    ${
-                        selectedSize === size
-                            ? "border-orange-500"
-                            : "bg-white text-gray-800 border-gray-400 hover:bg-gray-100"
-                    }`}
-                >
-                    {size}
-                </div>
-            </label>
-            
-        ))}
-        </div>
-        <button className=" mt-8 bg-black text-amber-100 w-[150px] h-[40px] cursor-pointer"> Add To Cart </button>
-        <hr className="mt-10 font-extralight"/>
-        <div className="mt-3 text-sm font-extralight">
-            <p> 100% Original product.</p>
-            <p>Cash on delivery is available on this product.</p>
-            <p>Easy return and exchange policy within 7 days.</p>
-        </div>
-         </div>
-        </div>
 
                 </div>
             </div>
