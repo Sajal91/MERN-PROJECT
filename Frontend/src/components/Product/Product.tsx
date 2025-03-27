3b6f9b6e09709e533197712a2ff45928b556a3f8
Sajal Kapoor <kapoorsajal91@gmail.com>
2025-03-28 00:23:46 +0530

    Updates: 1. env file changed to env.local file 2. Product Page Issue Fixed

--- Frontend/src/components/Product/Product.tsx
+++ Frontend/src/components/Product/Product.tsx
@@ -10,7 +10,7 @@ const Product: FC = () => {
     const { productId } = useParams<string>()
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [productdata, setProductdata] = useState<ProductDataInterface>()
-    const [productImage, setProductImage] = useState<string>("")
+    const [productImage, setProductImage] = useState<string>()
     const [isProductImageLoaded, setIsProductImageLoaded] = useState<boolean>(true);
     const [selectedSize, setSelectedSize] = useState<string>("");
     const [relatedCollection, setrelatedCollection] = useState<ProductDataInterface[]>()
@@ -29,44 +29,14 @@ const Product: FC = () => {
     }, [])
 
     const onProductImage = (image: string) => {
-        setIsProductImageLoaded(false)
-        setProductImage("")
-        setTimeout(() => {
-            setIsProductImageLoaded(true)
-            setProductImage(image)
-        }, 1000);
+        setIsProductImageLoaded(true)
+        setProductImage(image)
     }
 
     return (
         <>
             <div className="absolute top-0 left-0 pt-24 left w-full h-screen overflow-scroll scroll-smooth">
                 <div className="absolute left-1/2 -translate-x-1/2 w-[85vw] flex justify-between">
-                    <div className="flex w-[full] ">
-                        <div className="flex w-1/2">
-                            <div>
-                                {
-                                    productdata?.Product_Image &&
-                                    productdata.Product_Image.map((image, index) => {
-                                        return <div key={index}>
-                                            <img src={image} alt={productdata.Product_Name} className="bg-gray-200 h-[100px] w-[150px] overflow-hidden" onClick={() => onProductImage(image)} />
-                                        </div>
-                                    })
-                                }
-                            </div>
-                            {
-                                isProductImageLoaded ?
-                                    <div className="h-[320px] w-[250px]">
-                                        <img src={productImage} alt="Selected Product" className="w-full h-full bg-white p-5" />
-                                    </div>
-                                    : <div className="h-[320px] w-[250px] animate-pulse"></div>
-                            }
-                        </div>
-                        <div className="w-1/2">
-                            <h3>{productdata?.Product_Name}</h3>
-                            <h6>${productdata?.Product_Price}</h6>
-                            <p>{productdata?.Product_Description}</p>
-                        </div>
-                    </div>
                     <div className="flex w-[full] flex-wrap justify-center">
                         <div className="flex w-1/2 flex-wrap">
                             <div>
@@ -82,7 +52,6 @@ const Product: FC = () => {
                             </div>
                             <div >
                                 <img src={productImage} alt="Selected Product" className="h-[60vh] w-[25vw]  bg-white p-5" />
-
                             </div>
                         </div>
                         <div className="w-1/2 max-w-[600px]">
@@ -101,13 +70,7 @@ const Product: FC = () => {
                                             checked={selectedSize === size}
                                             onChange={(e) => setSelectedSize(e.target.value)}
                                             className="hidden"
-                                        /><div
-                                            className={`w-12 h-12 flex items-center justify-center border-2 text-lg font-semibold cursor-pointer transition-all 
-                    ${selectedSize === size
-                                                    ? "border-orange-500"
-                                                    : "bg-white text-gray-800 border-gray-400 hover:bg-gray-100"
-                                                }`}
-                                        >
+                                        /><div className={`w-12 h-12 flex items-center justify-center border-2 text-lg font-semibold cursor-pointer transition-all ${selectedSize === size ? "border-orange-500" : "bg-white text-gray-800 border-gray-400 hover:bg-gray-100"}`}>
                                             {size}
                                         </div>
                                     </label>
